3589fa5c3b88405736bfc06da18fbe3a723f6aad
mimodsg <alvaro.mena>
2026-05-27 08:41:07 -0500

    add floating cta rail

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import { FloatingCtaRail } from '@/components/organisms/FloatingCtaRail';
 import { FloatingNavigationRail } from '@/components/organisms/FloatingNavigationRail';
 import { HomePage } from '@/components/templates/HomePage';
 import { homePageContent } from '@/data/homePage';
@@ -5,6 +6,13 @@ import { homePageContent } from '@/data/homePage';
 export function App() {
   return (
     <>
+      <FloatingCtaRail
+        contactHref={homePageContent.floatingCtaRail.contactHref}
+        contactLabel={homePageContent.floatingCtaRail.contactLabel}
+        downloadFileName={homePageContent.floatingCtaRail.downloadFileName}
+        downloadHref={homePageContent.floatingCtaRail.downloadHref}
+        downloadLabel={homePageContent.floatingCtaRail.downloadLabel}
+      />
       <FloatingNavigationRail
         items={homePageContent.navigation.items}
         statusLabel={homePageContent.navigation.statusLabel}
